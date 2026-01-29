005204dd74dfec4bf7db0e77508f0c32c500a0d4
Esteban Romero <estebanromero124@gmail.com>
2026-01-29 13:08:50 -0500

    feat: Configure continuous deployment to GitHub Pages by adding a workflow, deployment scripts, and Astro site settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://EstebannRomeroo.github.io',
+  base: '/astro-landing-delete',
   vite: {
     plugins: [tailwindcss()]
   }
